47dffca1798d1b0b88fa9815d35d0e60784cd034
ShailjaNigam <92869831+ShailjaNigam@users.noreply.github.com>
2024-03-04 13:40:11 +0530

    Update Navbar.js
    commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,12 +4,43 @@ import './Navbar.css';
 import { languageOptions } from '../languageOptions';
 
 const Navbar = ({ userLang: language, setUserLang: setLanguage, userTheme,
-	setUserTheme, fontSize, setFontSize }) => {
+	setUserTheme, fontSize, setFontSize, code }) => {
 	const languages = languageOptions;
 	const themes = [
 		{ value: "vs-dark", label: "Dark" },
 		{ value: "light", label: "Light" },
 	]
+
+		// Function to handle code file download
+    const downloadCodeFile = () => {
+        let fileExtension = ".txt"; // Default file extension
+        
+        // Determine file extension based on selected language
+        switch (language.value) {
+            case "c":
+                fileExtension = ".c";
+                break;
+            case "cpp":
+                fileExtension = ".cpp";
+                break;
+            case "java":
+                fileExtension = ".java";
+                break;
+            case "python":
+                fileExtension = ".py";
+                break;
+            default:
+                fileExtension = ".txt"; // Default to .txt if language is not recognized
+                break;
+        }
+
+        const element = document.createElement("a");
+        const file = new Blob([code], {type: 'text/plain'});
+        element.href = URL.createObjectURL(file);
+        element.download = `code${fileExtension}`; // Set file name with extension
+        document.body.appendChild(element); // Required for this to work in Firefox
+        element.click();
+    };
 	return (
 		<div className="navbar">
 			<h1>Code Editor</h1>
